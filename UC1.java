a2fc844432887b4c0d7c6a7ed1d20a8eacc0175a
Naresh <pnareshsatya@gmail.com>
2021-06-28 12:12:52 +0530

    UC1

--- UC1.java
+++ UC1.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
 public class UC1{
-	public static void main(String[] args){
-		int totalSal = 0;
-		Scanner sal = new Scanner(System.in);
-		System.out.println("Enter the salary per day: ");
-		int daySal = sal.nextInt();
-		int fullday= 1;
-		System.out.println("Welcome");
-		for(int i=1;i<=20;i++){
-		double check = Math.floor(Math.random()*10)%2;
-		if(check == fullday){	
-			totalSal = totalSal+daySal;
+        public static void main(String[] args){
+                System.out.println("Welcome");
+                int fullday= 1;
+                System.out.println("Welcome");
+                for(int i=1;i<=20;i++){
+                double check = Math.floor(Math.random()*10)%2;
+                if(check == fullday){
+                        System.out.println("present");
 
-		}}
-		System.out.println(totalSal);
-	
+                }else{
+                        System.out.println("absent");
+                        }
+                }
 }
 }
