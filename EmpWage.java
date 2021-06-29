a6a31424d2e0824ea08577dea79b8cfedb6a943b
Naresh <pnareshsatya@gmail.com>
2021-06-29 12:38:59 +0530

    UC1

--- EmpWage.java
+++ EmpWage.java
@@ -1,13 +1,12 @@
 public class EmpWage{
 	public static void main(String args[]){
 		System.out.println("Welcome to EmpWage");
-                int isPresent = 1;
-                double check = Math.floor(Math.random()*10)%2;
-                if(check == isPresent){
-                        System.out.println("Employee is present");
-
-                }else{
-                      System.out.println("Employee is absent");
-                 }
+		int isPresent = 1;
+		double check = Math.floor(Math.random()*10)%2;
+		if(check == isPresent){
+			System.out.println("Employee is present");
+		}else{
+			System.out.println("Employee is absent");
+		}
 	}
 }
