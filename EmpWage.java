41402694b1337e143f47d02c38b453780cb897f1
Naresh <pnareshsatya@gmail.com>
2021-06-29 12:25:35 +0530

    UC4

--- EmpWage.java
+++ EmpWage.java
@@ -1,21 +1,23 @@
 public class EmpWage{
+                public static final int is_Part_Time = 1;
+                public static final int is_Full_Time = 2;
 	public static void main(String args[]){
 		System.out.println("Welcome to EmpWage");
-					 int wagePerhr = 20;
-					 int empHrs = 0;
-					 int dailyWage = 0;
-					 int is_Part_Time = 1;
-					 int is_Full_Time = 2;
-                double check = Math.floor(Math.random()*10)%3;
-                if(check == is_Full_Time){
-                        empHrs = 8;
-                }else if(check == is_Part_Time){
-								empHrs = 4;
-					 }
-					 else{
-								empHrs = 0;
-                 }
-                dailyWage = empHrs * wagePerhr;
-					 System.out.println("EmpWage = " + dailyWage);
-}
+		int wagePerhr = 20;
+		int empHrs = 0;
+		int dailyWage = 0;
+		int check =(int) Math.floor(Math.random()*10)%3;
+		switch(check){
+		case(is_Part_Time):
+			empHrs = 4;
+			break;
+		case(is_Full_Time):
+			empHrs = 8;
+			break;
+		default :
+			empHrs = 0;
+		}
+		dailyWage = empHrs * wagePerhr;
+		System.out.println("EmpWage = " + dailyWage);
+	}
 }
